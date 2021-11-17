3f520124ed8319b2b380faf4f5f4e1e29ad295f2
jefranca <jean13franca@gmail.com>
2021-11-17 18:21:37 -0300

    prepare dor deploy

--- src/database/database.js
+++ src/database/database.js
@@ -4,12 +4,12 @@ import '../setup.js'
 const { Pool } = pg
 
 let databaseConfig = {
-		connectionString: process.env.DATABASE_URL,
-		ssl: {
+	connectionString: process.env.DATABASE_URL,
+	ssl: {
 			rejectUnauthorized: false
-		}
 	}
+}
 
-	const connection = new Pool(databaseConfig)
+const connection = new Pool(databaseConfig)
 
 export default connection
